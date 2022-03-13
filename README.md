322795a818bedb93b481c1680ca4fedb1642cfc8
Aleks <58r1x7@gmail.com>
2022-03-13 21:25:30 +0300

    added two variants of script

--- README.md
+++ README.md
@@ -2,13 +2,19 @@
 Autoclicker (script) to Mine, Claim, Repair and restore Energy for NFT Game (https://farmersworld.io/).
 Its free and 100% safe. You can check all code by yourself.
 
+## About scripts
+
+There are two files **farmersworld_default.js** and **farmersworld_with_members_cards.js**.
+1) If you don't have member cards or you want to claim everytime the claim is ready - choose **farmersworld_default.js**
+2) If you have member cards and you want to reduce CPU usage -  choose **farmersworld_with_members_cards.js**.  **VERY IMPORTANT NOTE** - for parameter **'const minAmountOfEnergy ='** in .js file you need to set value equal of your highest tool energy requirement * quantity of stored mines. For example, you have Mining Excavator and two silver member cards. To mine your resources you need minimum 133 * 5 = 665 energy. So you need to set parameter **const minAmountOfEnergy = 665** otherwise you will never claim you resources!!!
+
 ## How to use it
 
 - Go to https://farmersworld.io
 - Login to the game
 - Press F12
 - Find and click 'Console' tab
-- Copy and paste the code to console and then press 'Enter'
+- Copy and paste the code from **farmersworld_default.js** or **farmersworld_with_members_cards.js** file to console and then press 'Enter'
 - To stop the code you need to refresh the page (F5)
 
 ## Contacts
---
+++ farmersworld_with_members_cards.js
@@ -0,0 +1,102 @@
+(async () => {
+    // Settings
+    const delayDefault = 5 * 1000 // The current example is 5 seconds. I don't recomend to change it.
+    const delayAfterClick = 2 * 1000 // The current example is 2 seconds. I don't recomend to change it.
+    const minAmountOfEnergy = 665 // If value of current energy is equal or less, energy will be restored. You can set any value you want, BUT READ IMPORTANT NOTE ON MAIN PAGE.
+    const repairDurabilityPercent = 50 // Current value is percent. For example chainsaw will be repaired when it hit 450/900 durability, because (450/900)*100 = 50%. You can set any value you want.
+
+    // Click the 'map' button
+    let result = {}
+    const mapButton = document.querySelector('.navbar-group--icon[alt="Map"]')
+    mapButton.click()
+
+    // Infinity loop
+    while (true) {
+        for (let mapId = 0; mapId < 4; ++mapId) {
+            if (typeof result[mapId] === 'undefined') result[mapId] = {}
+            await new Promise((res) => setTimeout(res, delayDefault))
+            const map = document.querySelectorAll('.map-container-bg')[mapId]
+            if (map.style.filter === 'grayscale(1)') continue
+            map.click()
+            await new Promise((res) => setTimeout(res, delayAfterClick))
+            const itemsContainer = document.querySelector('section.vertical-carousel-container')
+            if (itemsContainer) {
+                const children = itemsContainer.children
+                for (child of children) {
+                    document.getElementById('root').click()
+
+                    // ENERGY RESTORE START
+                    const energy = document.querySelectorAll('.resource__group')[3]
+                    const energyCheck = energy.innerText.split('\n/')
+                    if (energyCheck[0] <= minAmountOfEnergy) {
+                        document.querySelector('.resource-energy--plus').click()
+                        await new Promise((res) => setTimeout(res, delayAfterClick))
+                        const energyToRestore = Math.floor((energyCheck[1] - energyCheck[0]) / 5)
+                        for (let i = 0; i < energyToRestore; i++) {
+                            document.querySelector('img[alt="Plus Icon"]').click()
+                        }
+                        Array.from(document.querySelectorAll("div.plain-button")).find(el => el.textContent == 'Exchange').click()
+                    }
+                    // ENERGY RESTORE END
+
+                    child.click()
+                    await new Promise((res) => setTimeout(res, delayAfterClick))
+
+                    // REPAIR START
+                    document.getElementById('root').click()
+                    const cardName = document.querySelector('div.info-title-name').innerText
+                    if (!cardName.includes('Member') && mapId === 0) {
+                        const repairButton = document.querySelectorAll('.info-section .plain-button')[1];
+                        const currentDurability = +document.querySelector('.card-number').innerText.split('/ ')[0]
+                        const maxDurability = +document.querySelector('.card-number').innerText.split('/ ')[1]
+                        const percentageRatio = (currentDurability / maxDurability) * 100
+                        if (percentageRatio <= repairDurabilityPercent) {
+                            repairButton.click()
+                            await new Promise((res) => setTimeout(res, delayAfterClick * 3))
+                        }
+                    }
+                    // REPAIR END
+
+                    // MINE OR CLAIM START
+                    document.getElementById('root').click()
+                    const mineClaimButton = document.getElementsByClassName('button-section set-height')[0]
+                    if (mineClaimButton.innerText === 'Claim') {
+                        mineClaimButton.click()
+                        await new Promise((res) => setTimeout(res, delayDefault * 2))
+                        while (!mineClaimButton.innerText === 'Countdown' || document.getElementsByClassName('plain-button short undefined')[0]) {
+                            const modalButton = document.getElementsByClassName('plain-button short undefined')[0]
+                            if (modalButton && modalButton.innerText.toUpperCase() === 'OK') {
+                                modalButton.click()
+                                await new Promise((res) => setTimeout(res, delayAfterClick))
+                            } else {
+                                document.getElementById('root').click() 
+                                await new Promise((res) => setTimeout(res, delayAfterClick))
+                            }
+                        }
+                    }
+                    if (mineClaimButton.innerText === 'Mine') {
+                        const currentStoredClaims = +document.querySelector('div.info-title-level').innerText[0]
+                        const maxStoredClaims = +document.querySelector('div.info-title-level').innerText[2]
+                        if (currentStoredClaims === maxStoredClaims) {
+                            mineClaimButton.click()
+                            await new Promise((res) => setTimeout(res, delayDefault * 2))
+                            while (!mineClaimButton.innerText === 'Countdown' || document.getElementsByClassName('plain-button short undefined')[0]) {
+                                const modalButton = document.getElementsByClassName('plain-button short undefined')[0]
+                                if (modalButton && modalButton.innerText.toUpperCase() === 'OK') {
+                                    modalButton.click()
+                                    await new Promise((res) => setTimeout(res, delayAfterClick))
+                                } else {
+                                    document.getElementById('root').click() 
+                                    await new Promise((res) => setTimeout(res, delayAfterClick))
+                                }
+                            }
+                        }
+                    }
+                    // MINE OR CLAIM END
+                }
+            }
+            mapButton.click()
+            await new Promise((res) => setTimeout(res, delayDefault))
+        }
+    }
+})()
